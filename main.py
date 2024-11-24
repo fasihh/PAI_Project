fe719ed47f2517bc81fece7b444cd9ccaa8ae252
fasihh <fhk.tcs@gmail.com>
2024-11-24 19:36:25 +0500

    removing main

--- main.py
+++
@@ -1,8 +0,0 @@
-from model.analysis import Analysis
-
-anal = Analysis()
-plots = anal.countplots()
-
-for plot in plots:
-    with open(f'{plot}.png', 'wb') as f:
-        f.write(plots[plot].getvalue())
