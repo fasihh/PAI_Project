6fe904fe73299f0a497a4888c89c05029c96db1a
Muhammed-Owais01 <workandrecovery@gmail.com>
2024-11-26 20:02:51 +0500

    creating

--- view/src/Components/Graphs.tsx
+++ view/src/Components/Graphs.tsx
@@ -1,13 +1,51 @@
 const Graphs = () => {
     return ( 
-        <div className="flex flex-col items-center justify-center bg-slate-500 py-7">
-            <div className="py-6 rounded-2xl">
-                <img src="/images/longgraph.png" className="rounded-lg" />
-            </div>
-            <div>
-                <div className="flex">
-                    <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/graph2.png" className="rounded-xl" /></div>
-                    <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/graph1.png" className="rounded-xl" /></div>
+        <div className="flex items-center justify-center bg-slate-500 py-7">
+            <div className="bg-gray-200 flex">
+                <div className="h-[1000px] w-[300px] bg-slate-950">
+                    {/* Dashboard Div */}
+                </div>
+                <div className="flex flex-col my-4">
+                    <div className="flex mb-6">
+                        <div className="w-[400px] max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_workclass.png" className="rounded-xl" /></div>
+                        <div className="w-[480px] max-w-[500px] max-h-[500px] mx-6"><img src="/images/histplots_age.png" className="rounded-xl" /></div>
+                    </div>
+                    <div className="flex mb-6">
+                        <div className="w-[400px] max-w-[500px] max-h-[500px] mx-6"><img src="/images/boxplots_age.png" className="rounded-xl" /></div>
+                        <div className="w-[400px] max-w-[500px] max-h-[350px] mx-6"><img src="/images/splits_countplots_workclass.png" className="rounded-xl h-[350px] w-[480px]" /></div>
+                    </div>
+                    <div className="w-[400px] max-w-[500px] max-h-[500px] mx-6"><img src="/images/heatmap_heatmap.png" className="rounded-xl" /></div>
+                    <div className="flex flex-col">
+                        
+                        {/* <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_education.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_marital_status.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_occupation.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_relationship.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_race.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_sex.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_native_country.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/countplots_income.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/histplots_education_num.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/histplots_capital_gain.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/histplots_capital_loss.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/histplots_hours_per_week.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/boxplots_education_num.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/boxplots_capital_gain.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/boxplots_capital_loss.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/boxplots_hours_per_week.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/splits_countplots_education.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/splits_countplots_marital_status.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/splits_countplots_occupation.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/splits_countplots_relationship.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/splits_countplots_race.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/splits_countplots_sex.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/splits_countplots_native_country.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/splits_countplots_income.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/confusion_confusion.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/classification_classification.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/roc_roc.png" className="rounded-xl" /></div>
+                        <div className="max-w-[500px] max-h-[500px] mx-6"><img src="/images/vsk_vsk.png" className="rounded-xl" /></div> */}
+                    </div>
                 </div>
             </div>
         </div>
